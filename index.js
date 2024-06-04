041aac200e0079d5986a20bd7a9ab6e5b921a605
Myo Min Latt <myominlatt.sbs@gmail.com>
2024-06-04 10:21:10 +0630

    Host your Nodejs (Chat Server) on Vercel Cloud || Free Tier || WhatsApp clone #24

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ io.on("connection", (socket) => {
 });
 
 app.route("/check").get((req, res) => {
-    return res.json("Your App is working fine");
+    return res.json("Your App is working fine on vercel");
 })
 
 server.listen(port, "0.0.0.0",() => {
